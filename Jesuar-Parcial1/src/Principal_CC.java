96a9ab830a71cd57e78c50d26774cec90bfc7aee
Jesuarzm <jesuarzambrano@hotmail.com>
2022-11-06 13:33:07 -0600

    Correcion de parcia1 optimizado

--- Jesuar-Parcial1/src/Principal_CC.java
+++ Jesuar-Parcial1/src/Principal_CC.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.text.AbstractDocument.BranchElement;
+
 public class Principal_CC {
 
-    static String[][] sizeCinema =  new String[10][10];
+    static String[][] Cinepolis =  new String[10][10];
     static Scanner datoIngresado = new Scanner(System.in);
     static ArrayList<Character> myAbc = new ArrayList<Character>();
     static String asientoSolicitado;
     
+    //Instrccion que llena el cine con asientos "Vacios"
     public static void vaciarCine(){
-    for (int i = 0; i < sizeCinema.length; i++) {
-        for (int j = 0; j < sizeCinema[i].length; j++) {
-            sizeCinema[i][j] = "✓";
+    for (int i = 0; i < Cinepolis.length; i++) {
+        for (int j = 0; j < Cinepolis[i].length; j++) {
+            Cinepolis[i][j] = "✓";
             }
         }
     }
@@ -24,26 +27,52 @@ public class Principal_CC {
         }
         return myAbc;
     }
-    /*public static void mostrarAsientos() {
-        for (int i = 0; i < sizeCinema.length; i++) {
+    //Instruccion que permite ver los asientos
+    public static void mostrarAsientos() {
+        for (int i = 0; i < Cinepolis.length; i++) {
 			//Ciclo Para Mostrar Filas
 			System.out.print("\n[ "+ myAbc.get(i) +" ]");
-			for (int j = 1; j <= sizeCinema.length; j++) {
+			for (int j = 1; j <= Cinepolis.length; j++) {
 				//Ciclo para mostar Columnas
-				System.out.print("[ "+myAbc.get(i)+j+" "+sizeCinema[i][j-1]+" ]");
+				System.out.print("[ "+myAbc.get(i)+j+" "+Cinepolis[i][j-1]+" ]");
 			}
 		}
 		System.out.println();
-	}*/
+	}
+    public static void validacionAsiento(String asientoIngresado){
+        try{
+            //Separar los datos ingresados entre letra y numero
+            char letter = asientoIngresado.charAt(0);
+            int number = Character.getNumericValue(asientoIngresado.charAt(1)) - 1;
+
+            //Cilco que busca la posicion a verficar
+            for(int i=0; i < Cinepolis.length ; i++){
+                //Si dectecta que la posicion esta en uso retorna un "Error"
+                if( letter == myAbc.get(i) && Cinepolis[i][number] == "x"){
+                    System.out.println("Asiento ocupado, intente con otro.");
+                    break;
+                }
+                //Si la posicion esta libre la toma
+                if(letter == myAbc.get(i)){
+                    Cinepolis[i][number]="x";
+                }   
+            }
+
+        }catch (Exception exception){
+
+        }
+        
+    }
     public static void main(String[] args){
         
-        vaciarCine();
         createAbc();
-        for (int i = 0; i < sizeCinema.length; i++) {
-			//Ciclo Para Mostrar Filas
-			System.out.print("\n[ "+ myAbc.get(i) +" ]");
-            
-		}
+        vaciarCine();
+        do{
+        mostrarAsientos();
+        System.out.println("Por favor indicar asiento a usar:");
+        asientoSolicitado = datoIngresado.nextLine();
+        validacionAsiento(asientoSolicitado);
+        }while(true);
     }
     
 }
