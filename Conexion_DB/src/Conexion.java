3f5955bb6f461d7c954bbb15403526ec9f1e00df
Jesuarzm <jesuarzambrano@hotmail.com>
2022-11-10 00:28:48 -0600

    Conexion MySQL en Java

---
+++ Conexion_DB/src/Conexion.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	
+	private static final String Controlador = "com.mysql.cj.jdbc.Driver";
+	private static final String host = "jdbc:mysql://sql9.freesqldatabase.com:3306/sql9556192";
+	private static final String userDB = "sql9556192";
+	private static final String passDB = "HZKGcpWkTq";
+	
+	static {
+		try {
+			Class.forName(Controlador);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error Cargar Controlador");
+			e.printStackTrace();
+		}
+	}
+	public Connection conectar() {
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection(host,userDB,passDB);
+			System.out.println("Conexion Ok");
+
+		} catch (SQLException e) {
+			System.out.println("Error Conexion DB");
+			e.printStackTrace();
+		}
+		return conexion;
+	}
+
+}
